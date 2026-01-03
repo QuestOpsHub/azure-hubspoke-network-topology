31f644c3907540b1f2f4e0a94729974d2e5683be
Veera Bhadra Mangunuru <veerabhadra.devops@gmail.com>
2026-01-02 22:11:24 -0600

    test

--- deployments/spoke/data.tf
+++ deployments/spoke/data.tf
@@ -1,1 +1,10 @@
+#-----------------
+# Storage Account
+#-----------------
+data "azurerm_storage_account" "storage_account" {
+  provider            = azurerm.hub
+  name                = "stp01tfbackend9434"
+  resource_group_name = "rg-p01-tf-backend"
+}
+
 data "azurerm_client_config" "current" {}
